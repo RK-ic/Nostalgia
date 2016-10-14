341aaad1d6faf2747a82be809058b4f86d3b95e2
Chris Rohr <chrisdrohr@gmail.com>
2016-10-14 09:56:45 -0700

    photo resize

--- app/src/main/java/com/example/gabekeyner/nostalgia/Adapter.java
+++ app/src/main/java/com/example/gabekeyner/nostalgia/Adapter.java
@@ -38,7 +38,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @Override
     public void onBindViewHolder(final Adapter.ViewHolder holder, final int position) {
         holder.mTextView.setText(images.get(position).getImageHelper_name());
-        Picasso.with(context).load(images.get(position).getImageHelper_url()).into(holder.mImageView);
+
+        Picasso.with(context)
+                .load(images.get(position)
+                        .getImageHelper_url())
+                .resize(800, 500)
+                .centerCrop()
+                .into(holder.mImageView);
+
         //FOR ANIMATION
         if(position > previousPosition){
             //We are scrolling down
