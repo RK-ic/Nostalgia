bbd0a1f1a45a23cbe853edddbe33d866c8a4d5de
Chris Rohr <chrisdrohr@gmail.com>
2016-10-19 12:37:22 -0700

    adds splash screen

--- app/src/main/java/com/example/gabekeyner/nostalgia/SplashScreen.java
+++ app/src/main/java/com/example/gabekeyner/nostalgia/SplashScreen.java
@@ -1,45 +1,59 @@
 package com.example.gabekeyner.nostalgia;
 
 import android.content.Intent;
-import android.graphics.PixelFormat;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class SplashScreen extends AppCompatActivity {
-
-    VideoView videoView;
-
+    Animation fade_in, upfade;
+    ImageView imageView;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        fade_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
 
+        upfade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.upfade);
+        imageView = (ImageView) findViewById(R.id.imageSplash1);
+        textView = (TextView) findViewById(R.id.textViewSplash);
 
-        getWindow().setFormat(PixelFormat.TRANSLUCENT);
-        VideoView videoHolder = new VideoView(this);
-//if you want the controls to appear
-        videoHolder.setMediaController(new MediaController(this));
-        Uri video = Uri.parse("android.resource://" + getPackageName() + "/"
-                + R.raw.t); //do not add any extension
-//if your file is named sherif.mp4 and placed in /raw
-//use R.raw.sherif
-        videoHolder.setVideoURI(video);
-        setContentView(videoHolder);
-        videoHolder.start();
+        textView.setVisibility(View.INVISIBLE);
+
+        Picasso.with(this)
+                .load("http://wpwallpaper.com/wp-content/uploads/wallpaper/AutumnPath.jpg")
+                .fit()
+                .into(imageView);
+        imageView.startAnimation(fade_in);
+
 
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                textView.startAnimation(upfade);
+            }
+        },1500);
+
+
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 finish();
             }
-        }, 1000);
+        }, 4000);
 
     }
 
