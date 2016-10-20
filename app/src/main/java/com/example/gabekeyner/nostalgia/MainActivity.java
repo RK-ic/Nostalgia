a21f83813dc27c1bbfb025cf80dfc13fce5d030e
Gabe Keyner <holyground32@gmail.com>
2016-10-20 09:23:51 -0700

    added pictures to view

--- app/src/main/java/com/example/gabekeyner/nostalgia/MainActivity.java
+++ app/src/main/java/com/example/gabekeyner/nostalgia/MainActivity.java
@@ -74,13 +74,15 @@ public class MainActivity extends AppCompatActivity
     private Uri mMediaUri;
 
     private final String image_names[] = {
-            "Chris",
-            "Image",
-            "Image",
-            "Image",
-            "Image",
-            "Image",
-            "Image",
+            "City",
+            "ADI",
+            "Friends",
+            "Family",
+            "Birthday",
+            "Adventure",
+            "Hanging Out",
+            "Board Walk",
+            "Wall-E",
             "Image",
             "Image",
             "Image",
@@ -123,16 +125,17 @@ public class MainActivity extends AppCompatActivity
 
     private final String image_urls[] = {
 
-            "gs://nostalgia-2dd3f.appspot.com/Photos/image:26787",
+
             "http://www.hiltonhotels.de/assets/img/destinations/China/china-3.jpg",
-
-            "https://lh3.googleusercontent.com/jRAbvlcfEQ9n4-v4gHy1PXaqyLb4YRcwUkGNK2EAXqw-AmRKD9TUKwTv_clO22qt5qnZFRl49jeQUK4TgiWIs4YsUWGEM-Kea0TxIML5OZdpWFYtGDVTcmILSM3Db_4OyC6M8tNriXEY_LMfZHwNKS_GkYKE-ZQzxiemIbH4L53bbjEVz9kwgt_qZb9fJ7h4S13f_VQZjoMjTleeBbNzP0dqtkDomkX3KL-FyKA8-Pki1Nib9fcpK_uY1Mby9DW6H0v4sZ8mGhmfdBuLGttyJD_tg81LSe-DLsYpoJPbqbwHBXDexGNo9Zo2939UulZ6Z1DihDcfhVWRxVjS6TDBo0mglkQoQBfocFyvE-hOvhyMEhzbKfc_DN8_h3Xaj_zp19qNLyP2TmY7A6U-Y3zoUCcdkXYCled-ary6dgMpmB7lry7viS6B7sko77VoL9lOUOnfTbo6--zLNZclkDqmRA6LCPhXazicHMaGOlst_4oGdhaagHwWvA0wwuAGKFQkevbNncQIp6OhnMZYtZPUtniiWXeOkgI4l1a1vf8mBKifhp3CVaJmgdrQh7GLSxRdM2jK1IAp-3lSqwBGwjk7qXAXudAgqUZAz6lA_ZcZnJ5WgP-Llg=w1279-h960-no",
-
-            "http://kingofwallpapers.com/city-pictures/city-pictures-001.jpg",
-            "https://newevolutiondesigns.com/images/freebies/city-wallpaper-11.jpg",
-            "http://kingofwallpapers.com/city-pictures/city-pictures-021.jpg",
-            "http://photos.mandarinoriental.com/is/image/MandarinOriental/excelsior-exterior-home?$HomepageHeroImage$",
-            "http://best-wallpaper.net/wallpaper/1920x1200/1109/Beautiful-abstract-flight-line_1920x1200.jpg",
+   "https://lh3.googleusercontent.com/EItotXzxREzjDLufjmG1gj4_R9EHpDfXti49n7cJZzBBUucqWdeqIBEL0QwAWjnHhgiunMr0mA0tvbR5L-SZFORAT4WbTmfARLQV9EMVYfJkwq-IaVaN8DY8fgRsmMSXuBKdYb7_rwbbGxIs4ABY4Ldea3m32_ZoiYvrTlpJWVxIm-bQNhfE6pPM2Df0-Djv0rP-mh4U-EOY8TX4cKxFt6S9PA1EtLQV9x2zfj1D7ofEo89ynHiUMeC3SAexN74tZEbmkormZuzO_3SHXyqj1I8ovZkYQoa0ORQUf4rzX4M0N3KGdUPchpOTzWMJY3RvN8JZUL-R0OIP68-95HcDIdA_fprlmRhbuo2CqEPz-HLTAag3qz69P_OjGsunyMY6Pdx04Iyh3pKmknsawsazAXOch55_0gPlmgxToOLkI9kwHbt5Qpd9PEmDf374CJKEfH-b3KvDTgukF839_yB_IoD1ohBXTL1AhE4T0qocqQXChCZTTV0I00VutUmVkK4KXDM7SVSuTKBX68de_i_GAA-_oCkKrdu5QHd39OjrndUY-zKqNehKT7MFKA_OLAPWgFcuC3gFrwWfMWI3PTD30zYGou6opJvRXYgR3F5EfZIIvO_G=w1067-h643-no",
+            "https://lh3.googleusercontent.com/pVCbNVUveYcQYJiSUPFYLJK6nWWffZKNf7GCfJo9fPkOu7gO7wDLcEfKwNIu1dvExWNtkldaT4L9cJ3XZ88wM51XXX8of4zjBvVuVTUZoiMLs1k22v_31qiyNVBelS6tjjcBTsMHBbkHjvcFGs2x4hZmg1Rf4dW3UjSSxb-d4nrYmu3gpWaA3zOcMhmeeUhbuCmm-75Tpd9OETpmrfdC6umOTmwNwxbVgVaky_7bTAz_GnMfjEtFfTmO5wCSSRH98eTBjqjQG0OVPtcEcaa9n0SK3gH9Y_TXstCvmyg2GS94CRzIh7DzJKAO5XDvmrL4ZmdD2tbPVzm_XoYR2DmpIqJITbBLDv0vUtGdhCT02zix7Pe4PMCyRte5ajMEr9uxyAdgBVQW40dj3gJcjkyvlbM1dM-uHEKB70f5R1l7oVuKWpKngBZijNQwyxr4bp1FP9aCF70t07-CVKD5yz_0rSuwfjIiM6KDQkFwDRxLxGd0wid7B5LT_8KWDspUg13GTwXDm38rQGDvYkXOLtphZmeYyV6ViAXjWhY2O-0fxDheZMO5fGbEnDLXhk8ZVxBupExLvBp9nUQIh5IU7FsUAPj6_-WXHtHpP7RJxaCLF6ARrz7b=w1022-h1108-no",
+            "https://lh3.googleusercontent.com/g4CakSqZ7f7t9Y_uLLPPtsSmo17zmGRdR7P7nKqEc7DPJQSre_X4GSOoFmjq1TNj12NOB4R5zY_zBWs4pp7LDfC0yovHjxc7YWTDVvCREgyncPB66-WcQ8Xi6CDph1GUApNj21TvCLg-2xkro7zQkbckFfW0R58sTT7xW8CgJUN4g3eDCzQ3C-qWLLEpyyXB6ddd4gj2uIiOUTODx6XyGRTxQSsYFQYBRhRR6XBjl4yS1Yb48r6NSG-q2ThJxDk0qjgWlcoVa2DcXGnsg1-oL-lkGL6DDg-VJi7pqyxWyLvQpqypAZHziDV0RMREDXiMtwT0N406ponbgJWRHmXIxiJVBuN9AeA7mLJkAO4AMXSWrBMjuJ4fzK6b3ypbIgYka_D3ErpGJPet1_h-z0oFHY24avv6m4ujwT1ewJ8a_rsdpIzJts_XTth2Dr2bmlQVQ9XDaKdlSLIzBzpsYXh5L-ESYQjXS8GH3ONWgV2T1PGzkzKJKTyltps8Mmxz0Y7ZFjG7dR6Ko911LlpT1qSYpnffPZc1QemCSX49CIer5XHbuEZ_ibJJsbUswAypgVIQG0p08YM38toDnL9icrv2oc4noUg4" +
+                    "-Pc3wcmynhBSRKbhidcq=w720-h722-no",
+            "https://lh3.googleusercontent.com/yCRYwb3XAiWCChfcNJjaNMycEJvs6BcYG4co3cFDP3S3xbv9qXmzsGCGHcLiRV62cBUyhOyKbIPXefAtBgDyAoe8rbvSIf1TYchIOwFyGdlpXXL_ngOJj9zidFklqPJ80SWgtdaCXz8C1fwUE8bkBkTsIcb94t9AKet706gZh16uYbFDeTRBRF1sKae2YIBTIi6obaBSU-NDPfzGY3bicP1FDuNKWG3VWTgQa0t7uud247lM-mbPm2DUtGJKZAMXHyJfSI7rvqf99ZPh2G8v9wo4kpncdZ5Xfpvz8bDjefUbw5OcrZyMlznheiKRILsmEBJ7fRNLwcAKp7S4ohWfyJZYPsI9mpIPiDZlcIQ9p2gUWYpfQv87f2lMnreG3wViHza240LPA_8GQBkJz5doEArj16-_JB0WFlfzAOhEfcnIpQoA1BooSe--StpFj8w_0Iz24T7CVwLssd8nrHW4Ck4ptSKN1RgPYw247N-vNoqXPxuZC86i6j-TtlOP1Qamk1MYz1tYc_3WZ92-V52N1rpa0Skv9NS2aqZ-yb6s49MIeRsTVopbn1-o1NPXcUMB14HtBX95l5mBNgFm_np0r4qIZzc06fPE5_QQoVsiPm9gE8z8=w1067-h601-no",
+           "http://24.media.tumblr.com/812633100cc8a167f4875cf39099e743/tumblr_mexa6oloEB1qzyjdbo1_1280.jpg",
+            "http://67.media.tumblr.com/dd42b18d499fb9db2c9772c6efb34650/tumblr_mrhogsZc2S1qzyjdbo1_1280.jpg",
+            "https://images.freecreatives.com/wp-content/uploads/2015/05/Vintage-Photography-46-HD-Wallpaper.jpg",
+           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5uSKFBAiwMgdiGvEre3qWwRCfAQOfWxPvUY8_DH0GBbGJs7zQ",
             "http://lh5.ggpht.com/_Gq1jO6iuU2U/TTtbe7YWMAI/AAAAAAAAHfE/wq8EJ6fc4Yk/s9000/Abstract%2BTunnel%2BVision%2BHD%2BWallpaper.jpg",
             "http://cdn.wallpapersafari.com/4/13/4n5qmQ.jpg",
             "http://www.rajeshagrawal.com/wp-content/uploads/2015/02/london-at-night-desktop-wallpaper-beautiful-london-city-wallpapers-with-hd-gallery2.jpg",
